ab12232924e87dc3e65985f10e4be2618c4cd8e6
Uncovery <github@aimba.com>
2023-09-12 17:16:42 +0800

    fix function call

--- stripe.inc.php
+++ stripe.inc.php
@@ -6,12 +6,12 @@ if (!defined('WPINC')) {
 
 /**
  * we need to check if the stripe login works before we can process stuff
- * 
+ *
  * @return bool
  */
 function stripe_test_login() {
     $result = stripe_curl_command('products');
-    
+
     if (isset($result->error)) {
         echo user_alert("STRIPE CONNECTION ERROR: " . $result->error->{'message'});
         return false;
@@ -21,28 +21,28 @@ function stripe_test_login() {
 }
 
 /**
- * 
+ *
  * @param type $product_name
  * @param type $description
  * @param type $images
  */
 function stripe_create_product(string $product_name, string $description = '', array $images = []) {
     global $UMS;
-    
+
     $data = array(
         'name' => $product_name,
         'shippable' => 'false',
-        'statement_descriptor' => $UMS['statement_descriptor'], 
+        'statement_descriptor' => $UMS['statement_descriptor'],
         'description' => $description,
         'images' => $images,
     );
- 
+
     // echo ums_debug_show($product_object);
     return stripe_curl_command('products', $data);
 }
 
 function stripe_query_product(string $product_id) {
-    
+
     return stripe_curl_command('products/' . $product_id);
 }
 
@@ -54,10 +54,10 @@ function stripe_create_price(string $product_id) {
         'currency' => 'hkd',
         'product' =>  $product_id,
     );
- 
+
     return stripe_curl_command('prices', $data);
 }
-    
+
 function stripe_create_payment_link(object $price_object) {
     $data = array(
         'line_items[0][price]' => $price_object->id,
@@ -85,19 +85,19 @@ function stripe_create_session(string $price_id) {
  * @return type
  */
 function stripe_list_products() {
- 
+
     return stripe_curl_command('products');
 }
 
 
 function stripe_get_session_data(string $session_id) {
-    
+
     return stripe_curl_command('checkout/sessions/' . $session_id);
 }
 
 /**
  * execute a stripe cURL command and return the result
- * 
+ *
  * @param string $path
  * @param type $data
  * @param type $request
@@ -105,9 +105,9 @@ function stripe_get_session_data(string $session_id) {
  */
 function stripe_curl_command(string $path, $data = array(), $request = false) {
     $K = stripe_keys();
-    
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, curl_fix_path('https://api.stripe.com/v1/' . $path));
+    curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/' . $path);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -130,13 +130,13 @@ function stripe_curl_command(string $path, $data = array(), $request = false) {
 
 /**
  * returns the stripe key and secret, depending of we are live or testing
- * 
+ *
  * @global type $UMS
  * @return type
  */
 function stripe_keys() {
     global $UMS;
-    
+
     $key_data = array(
         'live' => array(
             'secret' => $UMS['stripe_api_secret_key'],
@@ -145,10 +145,10 @@ function stripe_keys() {
         'test' => array(
             'secret' => $UMS['stripe_api_test_secret_key'],
             'url' => 'https://dashboard.stripe.com/test/',
-        ),        
+        ),
     );
-    
+
     $mode = $UMS['stripe_mode'];
-    
+
     return $key_data[$mode];
 }
